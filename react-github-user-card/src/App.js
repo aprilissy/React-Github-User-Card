1c6025266ac49052c13653372298e1df05c6b13f
April Darger <april.m.darger@gmail.com>
2020-10-27 16:00:31 -0600

    making user searchable with modified axios and added input for form. Also installed semantic ui so I can try playing with cards.

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import axios from 'axios'
 import './App.css';
+import './components/UserCard'
 
 class App extends React.Component {
   state = {
-    user:{},
+    userData:{},
+    userName:''
   }
 
   fetchUser = (user) => {
-    axios.get(`https://api.github.com/users/aprilissy`)
+    axios.get(`https://api.github.com/users/${user}`)
       .then(res => {
         console.log('axios response', res);
         this.setState({
-          user:res.data
+          userData:res.data
         })
       })
       .catch(err => {
@@ -21,10 +23,30 @@ class App extends React.Component {
       })
   }
 
+  handleSearch = (e) => {
+    e.preventDefault()
+    this.fetchUser(this.state.userName)
+    this.setState({userName:''})
+  }
+
+  handleChange = (e) => {
+    this.setState({userName:e.target.value})
+  }
+
   render() {
     return (
-      <div clasName='AppContainer'>
+      <div className='AppContainer'>
         <h1>Hello github</h1>
+        <form onSubmit={this.handleSearch}>
+          <input 
+            type='text'
+            value={this.state.userName}
+            onChange={this.handleChange}
+          />
+        </form>
+        <div className='UserCard'>
+          <UserCard userData={userData}/>
+        </div>
       </div>
     );
   }
